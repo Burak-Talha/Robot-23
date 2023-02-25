5fb9d05eca03ea14097128c727c83a61a305e3bc
Burak-Talha <alptng72@gmail.com>
2023-02-25 23:30:42 +0300

    Add PID gains, limits, motor Id's

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,11 @@ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 public class Constants {
 
     public static class DriveConstants{
+        public static final int MASTER_LEFT_ID = 0;
+        public static final int SLAVE_LEFT_ID = 1;
+        public static final int MASTER_RIGHT_ID = 2;
+        public static final int SLAVE_RIGHT_ID = 3;
+
         public static final double WHEEL_DIAMETER_CM = 50.24;
         public static final double ENCODER_PPR = 1024;
         public static final double REDUCTION_RATE = 0.25;
@@ -14,12 +19,32 @@ public class Constants {
     }
 
     public static class ArmConstants{
-        public static final double DEFAULT_ARM_LENGTH = 1.2;
+
+        // Shoulder
+        public static final int MASTER_ROTATABLE_ID = 4;
+        public static final int SLAVE_ROTATABLE_ID = 5;
+        public static final double SHOULDER_KP = 0;
+        public static final double SHOULDER_KI = 0;
+        public static final double SHOULDER_KD = 0;
+        public static final double SHOULDER_KF = 0;
+        public static final double SHOULDER_UP_LIMIT = 0;
+        public static final double SHOULDER_DOWN_LIMIT = 0;
         public static final double k_ARM_ANGLE_OFFSET = 30;
-        public static final double ARM_PPR = 48;
-        public static final double K_ARMTICK2DEGREE = 360 / ARM_PPR;
-        public static final double K_EXTENSIBLE_TICK2METER = 0;
+        public static final double SHOULDER_PPR = 48;
+        // Extensible
+        public static final int MASTER_EXTENSION_ID = 6;
+        public static final double MAX_ARM_LENGTH = 1.5;
+        public static final double DEFAULT_ARM_LENGTH = 1.2;
+        public static final double EXTENSIBLE_KP = 0;
+        public static final double EXTENSIBLE_KI = 0;
+        public static final double EXTENSIBLE_KD = 0;
+        public static final double EXTENSIBLE_KF = 0;
+        public static final double EXTENSIBLE_UP_LIMIT = 0;
+        public static final double EXTENSIBLE_DOWN_LIMIT = 0;
+        // Calculations
+        public static final double K_ARMTICK2DEGREE = 360 / SHOULDER_PPR;
         public static final double K_ARM_HEIGHT_M = 0.40;
+        public static final double K_EXTENSIBLE_TICK2METER = 0;
     }
 
     public static class VisionConstants{
