613b28262d93e72c1b66dfd6e4dc727cf636d6e6
okcu73 <alptng72@gmail.com>
2023-03-05 18:19:42 +0300

    Add new Blue and Red Target without 3d coordinates

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -1,17 +1,115 @@
 package frc.robot;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
+import frc.robot.lib.frc7682.Target;
+import frc.robot.lib.frc7682.TargetFinder.DesiredPosition;
+import frc.robot.lib.frc7682.TargetFinder.ObjectType;
+import frc.robot.lib.frc7682.TargetFinder.TargetType;
 
 public class FieldConstants {
 
     public static class LoadingZone{
         
-    }
+    public Rotation3d loadingZoneRotation = new Rotation3d(0, 0, 0);
 
+    // Target rows are designed by scoring table opposite
+        // Contains red alliance target positions
+    public static final List<Target> RED_TARGETS = Arrays.asList(
+                                                            // Cone targets
+                                                            // 1st row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 3nd row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 4th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 6th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 7th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 8th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+
+                                                            // Cube Targets
+                                                            // 2nd row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.UP),
+                                                            // 5th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.UP),
+                                                            // 9th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.UP),
+
+                                                            new Target(aprilTags.get(5), ObjectType.BOTH, TargetType.GET, DesiredPosition.UP)
+                                                            )
+                                                            ;
+    public static final List<Target> BLUE_TARGETS = Arrays.asList(
+                                                            // Cone targets
+                                                            // 1st row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 3nd row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 4th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 6th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 7th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+                                                            // 8th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CONE, TargetType.POST, DesiredPosition.UP),
+
+                                                            // Cube Targets
+                                                            // 2nd row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.UP),
+                                                            // 5th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.UP),
+                                                            // 9th row
+                                                            new Target(new Pose3d(), ObjectType.BOTH, TargetType.POST, DesiredPosition.DOWN),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.MID),
+                                                            new Target(new Pose3d(), ObjectType.CUBE, TargetType.POST, DesiredPosition.UP),
+
+                                                            new Target(aprilTags.get(6), ObjectType.BOTH, TargetType.GET, DesiredPosition.UP)
+                                                            );
+
+    }
+    // FMwill constants
     public static final Map<Integer, Pose3d> aprilTags =
       Map.of(
           1,
