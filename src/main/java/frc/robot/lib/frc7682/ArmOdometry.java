53d22d7bc56ea2af3f7c044bf21365a0b5522ef6
okcu73 <alptng72@gmail.com>
2023-03-11 02:24:04 +0300

    Import Changes

--- src/main/java/frc/robot/lib/frc7682/ArmOdometry.java
+++ src/main/java/frc/robot/lib/frc7682/ArmOdometry.java
@@ -1,6 +1,5 @@
 package frc.robot.lib.frc7682;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import frc.robot.Constants;
