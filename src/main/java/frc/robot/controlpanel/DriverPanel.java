2087eac25afd3d7c7b9359200204f07f352f57a3
Burak-Talha <alptng72@gmail.com>
2023-03-27 13:53:38 +0300

    Add reverseDrive

--- src/main/java/frc/robot/controlpanel/DriverPanel.java
+++ src/main/java/frc/robot/controlpanel/DriverPanel.java
@@ -6,6 +6,7 @@ import frc.robot.lib.frc7682.FusionJoystick;
 public class DriverPanel implements IDriverPanel{
 
     private FusionJoystick driverStick;
+    private boolean isReversed = false;
 
     DriverPanel(FusionJoystick driverStick){
         this.driverStick = new FusionJoystick(Constants.JoystickConstants.DRIVER_CONTROLLER_PORT);
@@ -16,14 +17,24 @@ public class DriverPanel implements IDriverPanel{
         driverStick.setSlewRate(0.5);
     }
 
+
     @Override
     public double speed() {
-        return driverStick.getRawAxis(1);
+        return driverStick.getRawAxis(1) * (isReversed ? -1 : 1);
     }
 
     @Override
     public double rotation() {
-        return driverStick.getRawAxis(4);
+        return driverStick.getRawAxis(4) * (isReversed ? -1 : 1);
+    }
+
+    @Override
+    public void reverseDrive() {
+        setReversed(!isReversed);
+    }
+    
+    private void setReversed(boolean isReversed){
+        this.isReversed = isReversed;
     }
 
 }
