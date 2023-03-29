a6cc2730ef95d6aa4d5791dcc45b9d20505e8ca0
okcu73 <alptng72@gmail.com>
2023-03-29 14:48:17 +0300

    Update April Tag Constants

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -1,13 +1,13 @@
 package frc.robot;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
 import frc.robot.lib.frc7682.Target;
 import frc.robot.lib.frc7682.TargetFinder.DesiredPosition;
 import frc.robot.lib.frc7682.TargetFinder.ObjectType;
@@ -15,67 +15,14 @@ import frc.robot.lib.frc7682.TargetFinder.TargetType;
 
 public class FieldConstants {
 
-        // FMwill constants
-        public static final List<AprilTag> aprilTags =
-        List.of(
-          new AprilTag(1,
-              new Pose3d(
-              Units.inchesToMeters(610.77),
-              Units.inchesToMeters(42.19),
-              Units.inchesToMeters(18.22),
-              new Rotation3d(0.0, 0.0, Math.PI))),
-              
-          new AprilTag(2,
-            new Pose3d(
-                Units.inchesToMeters(610.77),
-                Units.inchesToMeters(108.19),
-                Units.inchesToMeters(18.22),
-                new Rotation3d(0.0, 0.0, Math.PI))),
-
-          new AprilTag(3,
-            new Pose3d(
-                Units.inchesToMeters(610.77),
-                Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
-                Units.inchesToMeters(18.22),
-                new Rotation3d(0.0, 0.0, Math.PI))),
-
-          new AprilTag(4,
-            new Pose3d(
-                Units.inchesToMeters(636.96),
-                Units.inchesToMeters(265.74),
-                Units.inchesToMeters(27.38),
-                new Rotation3d(0.0, 0.0, Math.PI))),
-
-          new AprilTag(5,
-            new Pose3d(
-                Units.inchesToMeters(14.25),
-                Units.inchesToMeters(265.74),
-                Units.inchesToMeters(27.38),
-                new Rotation3d())),
-
-          new AprilTag(6,
-            new Pose3d(
-                Units.inchesToMeters(40.45),
-                Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
-                Units.inchesToMeters(18.22),
-                new Rotation3d())),
-
-          new AprilTag(7,
-          new Pose3d(
-              Units.inchesToMeters(40.45),
-              Units.inchesToMeters(108.19),
-              Units.inchesToMeters(18.22),
-              new Rotation3d())),
-
-          new AprilTag(8,
-          new Pose3d(
-              Units.inchesToMeters(40.45),
-              Units.inchesToMeters(42.19),
-              Units.inchesToMeters(18.22),
-              new Rotation3d()))
-      );
-
-    public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = new AprilTagFieldLayout(aprilTags, 16.5, 8);
+    public static AprilTagFieldLayout fieldLayout(){
+      try{
+        return AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
+      }catch(IOException exception){
+        exception.printStackTrace();
+      }
+      return null;
+    }
 
     public static class LoadingZone{
         
@@ -133,8 +80,8 @@ public class FieldConstants {
                                                             new Target(new Pose3d(new Translation3d(1.093, 1.293, 0.517), new Rotation3d()), ObjectType.UNKNOWN, TargetType.POST, DesiredPosition.MID),
                                                             new Target(new Pose3d(new Translation3d(0.647, 1.294, 0.820), new Rotation3d()), ObjectType.UNKNOWN, TargetType.POST, DesiredPosition.UP),
 
-                                                            new Target(aprilTags.get(5).pose.relativeTo(new Pose3d(0, 0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP),
-                                                            new Target(aprilTags.get(6).pose.relativeTo(new Pose3d(0, -0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP)
+                                                            new Target(fieldLayout().getTagPose(5).get().relativeTo(new Pose3d(0, 0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP),
+                                                            new Target(fieldLayout().getTagPose(5).get().relativeTo(new Pose3d(0, -0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP)
                                                             );
     public static final List<Target> RED_TARGETS = Arrays.asList(
                                                             // Cone targets
@@ -186,8 +133,8 @@ public class FieldConstants {
                                                             new Target(new Pose3d(new Translation3d(16.003, 1.275, 0.523), new Rotation3d()), ObjectType.UNKNOWN, TargetType.POST, DesiredPosition.MID),
                                                             new Target(new Pose3d(new Translation3d(16.447, 1.275, 0.826), new Rotation3d()), ObjectType.UNKNOWN, TargetType.POST, DesiredPosition.UP),
 
-                                                            new Target(aprilTags.get(6).pose.relativeTo(new Pose3d(0, 0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP),
-                                                            new Target(aprilTags.get(6).pose.relativeTo(new Pose3d(0, -0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP)
+                                                            new Target(fieldLayout().getTagPose(6).get().relativeTo(new Pose3d(0, 0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP),
+                                                            new Target(fieldLayout().getTagPose(6).get().relativeTo(new Pose3d(0, -0.675, 0.238, new Rotation3d())), ObjectType.UNKNOWN, TargetType.GET, DesiredPosition.UP)
                                                             );
     }
 }
